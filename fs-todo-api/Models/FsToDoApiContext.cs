af26438e79f7d1b54b165859bc1978e558f74aa0
Kymani Stephens <kymanist00@gmail.com>
2023-11-22 12:51:28 -0800

    fix seed data

--- fs-todo-api/Models/FsToDoApiContext.cs
+++ fs-todo-api/Models/FsToDoApiContext.cs
@@ -14,8 +14,7 @@ namespace FsToDoApi.Models
     {
       builder.Entity<ToDo>()
         .HasData(
-          new ToDo { ToDoId = 1, Title = "Wash Dishes", Description = "wash all the dishes in the sink!", Completed = false },
-          new ToDo { ToDoId = 2, Title = "Clean bathroom", Description = "clean the bathroom!", Completed = false }
+          new ToDo { ToDoId = 1, Title = "Wash Dishes", Description = "wash all the dishes in the sink!", Completed = false }
         );
 
     }
