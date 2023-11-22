36f55303885d1a7914d81a8fc0ddfc53f1f39527
Kymani Stephens <kymanist00@gmail.com>
2023-11-22 13:30:16 -0800

    add some dummy text

--- fs-todo-client/src/App.jsx
+++ fs-todo-client/src/App.jsx
@@ -3,6 +3,11 @@ function App() {
   return (
     <>
       <h1>This is just a test</h1>
+      <h2>Test app</h2>
+      <h3>Test app</h3>
+      <h4>Test app</h4>
+      <h5>Test app</h5>
+      <h6>Test app</h6> 
     </>
   )
 }
